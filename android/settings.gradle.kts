8d76a2420bd049c97eb652e1365bfd9c6d03165d
006niti-arch <006niti@gmail.com>
2025-08-29 14:07:09 +0530

    Definitive Fix: Correct modern Kotlin DSL for cloud build

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,16 +1,5 @@
 // android/settings.gradle.kts
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        // Corrected path for the new structure
-        file("../local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
@@ -21,7 +10,6 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.2.1" apply false
-    // Use this stable version for Google Services
     id("com.google.gms.google-services") version "4.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
